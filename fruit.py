94f43ccd63fa0dbbfa1468eaff03c821c4fbbb57
Rubén <98332317+marsi107@users.noreply.github.com>
2023-09-06 23:35:08 +0200

    define better limits fo the apple

--- fruit.py
+++ fruit.py
@@ -11,11 +11,13 @@ class Apple:
     max_x, max_y = 0, 0
 
     def __init__(self, max_x, max_y):
-        self.max_x = max_x
-        self.max_y = max_y
+        # set limit minus apple_size so it doesn't appear outside
+        self.max_x = max_x - self.APPLE_SIZE
+        self.max_y = max_y - self.APPLE_SIZE
         self.generate_new_apple_position()
 
     def generate_new_apple_position(self):
-        x = random.randint(0, self.max_x)
-        y = random.randint(0, self.max_y)
+        # set first limit as apple_size so it doesn't appear outside
+        x = random.randint(self.APPLE_SIZE, self.max_x)
+        y = random.randint(self.APPLE_SIZE, self.max_y)
         self.apple_pos = pygame.Vector2(x, y)
